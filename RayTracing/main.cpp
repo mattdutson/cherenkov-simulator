b0f7ba2685a37f2d4a03169194f349ed8dbd914f
Matthew Dutson <mattdutson@icloud.com>
2016-01-17 23:32:55 -0700

    Initial Setup

---
+++ RayTracing/main.cpp
@@ -0,0 +1,10 @@
+//  Created by Matthew Dutson on 1/17/16.
+//  Copyright © 2016 Matthew Dutson. All rights reserved.
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
