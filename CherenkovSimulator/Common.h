4794a24058f5e8d5a1f2779131a266e531f55020
Matthew Dutson <mattdutson@icloud.com>
2016-09-01 00:02:58 -0600

    Added "Prefix.pch" and "Common.h"
    "Prefix.pch" is a precompiled header which will be used for program-wide includes. "Common.h" is a header which will contain globally useful macros and the information pulled from the configuration file.

---
+++ CherenkovSimulator/Common.h
@@ -0,0 +1,13 @@
+//
+//  Common.h
+//  CherenkovSimulator
+//
+//  Created by Matthew Dutson on 8/31/16.
+//  Copyright © 2016 Matthew Dutson. All rights reserved.
+//
+
+#ifndef Common_h
+#define Common_h
+
+
+#endif /* Common_h */
