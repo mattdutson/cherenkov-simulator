8ec885b527c122a37612d5a1fcd4934d5338c191
Matthew Dutson <mattdutson@icloud.com>
2017-01-02 14:22:37 -0700

    Implemented basics of photon count data structure

--- cherenkov_library/data_containers.cpp
+++ cherenkov_library/data_containers.cpp
@@ -7,19 +7,51 @@
 
 #include "data_containers.h"
 
+using namespace std;
+
 namespace cherenkov_simulator
 {
 
-    PhotonCount::PhotonCount(FileOptions* config)
+    PhotonCount::PhotonCount(int pixel_count)
     {
-
+        photon_counts = vector<vector<vector<int>>>();
+        for (int i = 0; i < pixel_count; i++)
+        {
+            photon_counts.push_back(vector<vector<int>>());
+            for (int j = 0; j < pixel_count; j++)
+            {
+                photon_counts[i].push_back(vector<int>());
+            }
+        }
     }
 
-    void PhotonCount::AddPhoton(double time, int x_index, int y_index) {
-
+    void PhotonCount::AddPhoton(int time_slot, int x_index, int y_index)
+    {
+        if (!ValidPixel(x_index, y_index))
+        {
+            return;
+        } else
+        {
+            if (photon_counts[x_index][y_index].size() < time_slot)
+            {
+                photon_counts[x_index][y_index].resize(time_slot, 0);
+            }
+            photon_counts[x_index][y_index][time_slot]++;
+        }
     }
 
-    void PhotonCount::AddPoint(TVector3 direction, double time) {
-
+    bool PhotonCount::ValidPixel(int x_index, int y_index)
+    {
+        int n_pixels = photon_counts.size();
+        double center_index = (n_pixels + 1) / 2;
+        double x_dist = x_index - center_index;
+        double y_dist = y_index - center_index;
+        if (x_dist * x_dist + y_dist * y_dist > n_pixels * n_pixels / 4.0)
+        {
+            return false;
+        } else
+        {
+            return true;
+        }
     }
 }
--- cherenkov_library/data_containers.h
+++ cherenkov_library/data_containers.h
@@ -20,7 +20,7 @@ namespace cherenkov_simulator
         
         std::vector<std::vector<std::vector<int>>> photon_counts;
 
-        FileOptions* config;
+        bool ValidPixel(int x_index, int y_index);
         
     public:
 
@@ -53,11 +53,14 @@ namespace cherenkov_simulator
 
         };
 
-        PhotonCount(FileOptions* config);
-        
-        void AddPhoton(double time, int x_index, int y_index);
+        /*
+         * Creates a 2d indexed collection of vectors (a vector of vectors of vectors). Each vector represents the time
+         * series for a particular photomultiplier. The pixel_count parameter defines the width/height of the cluster in
+         * number of pixels.
+         */
+        PhotonCount(int pixel_count);
 
-        void AddPoint(TVector3 direction, double time);
+        void AddPhoton(int time_slot, int x_index, int y_index);
 
         TVector3 PixelDirection(int x_index, int y_index);
 
