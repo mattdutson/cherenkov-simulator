7712b512036030083fd617719e33f1dfa7d6f382
Matthew Dutson <mattdutson@icloud.com>
2017-01-08 18:56:08 -0700

    Filled out PhotonCount and SignalIterator classes
    Implemented various features of both classes, including the Next method of the SignalIterator and Poisson noise addition for the PhotonCount class.

--- cherenkov_library/data_containers.cpp
+++ cherenkov_library/data_containers.cpp
@@ -8,35 +8,98 @@
 #include "data_containers.h"
 #include "TMath.h"
 
-using namespace std;
 using namespace TMath;
+using std::vector;
 
 namespace cherenkov_simulator
 {
 
-    PhotonCount::PhotonCount(int n_pmt_across, double start_time, double time_bin, double pmt_angular_size)
+    SignalIterator::SignalIterator(std::vector<std::vector<bool>> validPixels)
     {
-        // Copy parameters over.
+        this->valid_pixels = validPixels;
+        Reset();
+    }
+
+    int SignalIterator::X()
+    {
+        return x_current;
+    }
+
+    int SignalIterator::Y()
+    {
+        return y_current;
+    }
+
+    bool SignalIterator::Next()
+    {
+        // If the outer vector is empty, then there will never be anything to iterate through.
+        if (valid_pixels.size() == 0)
+        {
+            return false;
+        }
+
+        // Loop until we find a valid pixel.
+        bool valid = false;
+        while (!valid)
+        {
+            // If we're at the end of the current column, try to move to the next column. Otherwise, increment y.
+            if (y_current == valid_pixels[x_current].size() - 1)
+            {
+                // If we're at the end of the last column, return false. Otherwise, move to the next column.
+                if (x_current == valid_pixels.size() - 1)
+                {
+                    return false;
+                }
+                else
+                {
+                    x_current++;
+                    y_current = -1;
+                }
+            }
+            else
+            {
+                y_current++;
+            }
+            if (y_current > -1)
+            {
+                valid = valid_pixels[x_current][y_current];
+            }
+        }
+        return true;
+    }
+
+    void SignalIterator::Reset()
+    {
+        x_current = 0;
+        y_current = -1;
+    }
+
+    PhotonCount::PhotonCount(int n_pmt_across, double start_time, double time_bin, double pmt_angular_size,
+                             double pmt_linear_size)
+    {
+        // Copy parameters.
         this->start_time = start_time;
         this->time_bin = time_bin;
         this->pmt_angular_size = pmt_angular_size;
+        this->pmt_linear_size = pmt_linear_size;
 
-        // Initialize the data structure.
+        // Initialize the photon count and valid pixel structures.
         photon_counts = vector<vector<vector<int>>>();
+        valid_pixels = vector<vector<bool>>();
         for (int i = 0; i < n_pmt_across; i++)
         {
             photon_counts.push_back(vector<vector<int>>());
+            valid_pixels.push_back(vector<bool>());
             for (int j = 0; j < n_pmt_across; j++)
             {
                 photon_counts[i].push_back(vector<int>());
+                valid_pixels[i].push_back(ValidPixel(i, j));
             }
         }
     }
 
     void PhotonCount::AddPhoton(double time, TVector3 direction)
     {
-        direction = -direction;
-
         // The angle of the yz projection with the z axis. See notes for details.
         double elevation = ATan(direction.Y() / direction.Z());
 
@@ -48,10 +111,7 @@ namespace cherenkov_simulator
         int x_index = Floor(azimuth / (pmt_angular_size * Cos(elevation))) + photon_counts.size() + 1;
 
         // If the location is valid, add the pixel to the underlying data structure.
-        if (!ValidPixel(x_index, y_index))
-        {
-            return;
-        } else
+        if (ValidPixel(x_index, y_index))
         {
             int time_slot = Floor((time - start_time) / time_bin);
             if (photon_counts[x_index][y_index].size() < time_slot)
@@ -62,20 +122,61 @@ namespace cherenkov_simulator
         }
     }
 
+    void PhotonCount::AddNoise(double noise_rate, SignalIterator current, TRandom3 rng)
+    {
+        // Calculate the number of noise photons per second from the number of photons per second per steradian per
+        // square centimeter.
+        double solid_angle = Sq(pmt_angular_size);
+        double area = Sq(pmt_linear_size);
+        double pixel_rate = noise_rate * solid_angle * area;
+
+        // Determine the mean number of Poisson events in a single time bin.
+        double expected_photons = time_bin * pixel_rate;
+
+        // Randomly determine the number of photons in each bin according to the Poisson distribution.
+        vector<int> data = photon_counts[current.X()][current.Y()];
+        for (int i = 0; i < data.size(); i++)
+        {
+            data[i] += rng.Poisson(expected_photons);
+        }
+    }
+
+    TVector3 PhotonCount::Direction(SignalIterator current)
+    {
+        // Reverse the operations of AddPhoton. Add 0.5 to account for the Floor() operation.
+        double elevation = (current.Y() - 1.0 - photon_counts.size() + 0.5) * pmt_angular_size;
+        double azimuth = (current.X() - 1.0 - photon_counts.size() + 0.5) * pmt_angular_size * Cos(elevation);
+
+        // TODO: Check that these rotations are in the correct direction (perhaps with unit testing).
+        TVector3 direction = TVector3(0, 0, 1);
+        direction.RotateX(elevation);
+        direction.RotateY(azimuth);
+    }
+
+    SignalIterator PhotonCount::Iterator()
+    {
+        return SignalIterator(valid_pixels);
+    }
+
+    int PhotonCount::SumBins(SignalIterator current)
+    {
+        vector<int> data = photon_counts[current.X()][current.Y()];
+        int sum = 0;
+        for (int count : data)
+        {
+            sum += count;
+        }
+        return sum;
+    }
+
     bool PhotonCount::ValidPixel(int x_index, int y_index)
     {
         int n_pixels = photon_counts.size();
         double center_index = (n_pixels + 1) / 2;
         double x_dist = x_index - center_index;
         double y_dist = y_index - center_index;
-        if (x_dist * x_dist + y_dist * y_dist > n_pixels * n_pixels / 4.0)
-        {
-            return false;
-        } else
-        {
-            return true;
-        }
+
+        // The radius of the valid circle is equal to half the number of valid pixels.
+        return Sq(x_dist) + Sq(y_dist) < Sq(n_pixels / 2.0);
     }
-
-
 }
--- cherenkov_library/data_containers.h
+++ cherenkov_library/data_containers.h
@@ -8,19 +8,108 @@
 #ifndef data_containers_h
 #define data_containers_h
 
-#include "utility.h"
 #include "TVector3.h"
+#include "TRandom3.h"
 
 namespace cherenkov_simulator
 {
 
-    class PhotonCount
+    class SignalIterator
     {
+
+    public:
+
+        /*
+         * The only constructor. Takes a 2D vector of bools which can be used to determine which pixels are valid. The
+         * user is responsible for ensuring that all sub-vectors are non-null.
+         */
+        SignalIterator(std::vector<std::vector<bool>> validPixels);
+
+        /*
+         * Returns the current x index of the iterator.
+         */
+        int X();
+
+        /*
+         * Returns the current y index of the iterator.
+         */
+        int Y();
+
+        /*
+         * Moves to the next photomultiplier signal. Returns false if the iterator has reached the end of the
+         * collection (the last photomultiplier).
+         */
+        bool Next();
+
+        /*
+         * Returns the iterator to its starting state.
+         */
+        void Reset();
+
     private:
 
-        // TODO: Pass this container information about the relationship between pixel directions and indices.
+        // A 2D vector of bools which can be used to determine which pixels are valid.
+        std::vector<std::vector<bool>> valid_pixels;
 
+        // The current x index of the iterator
+        int x_current;
+
+        // The current y index of the iterator
+        int y_current;
+    };
+
+    class PhotonCount
+    {
+
+    public:
+
+        /*
+         * Creates a 2d indexed collection of vectors (a vector of vectors of vectors). Each vector represents the time
+         * series for a particular photomultiplier. Takes the width/height of the array in number of photomultipliers,
+         * the starting time for the series, the width of a time bin, and the angular width of a photomultiplier.
+         */
+        PhotonCount(int n_pmt_across, double start_time, double time_bin, double pmt_angular_size,
+                    double pmt_linear_size);
+
+        /*
+         * Increments the photon count at some time for the photomultiplier pointing in the specified direction.
+         */
+        void AddPhoton(double time, TVector3 direction);
+
+        /*
+         * Adds some number of noise photons to the channel referenced by the iterator. The noise rate represents the
+         * number of photons per second per steradian per square centimeter. These photons are randomly scattered
+         * throughout the time bins using the random number generator.
+         */
+        void AddNoise(double noise_rate, SignalIterator current, TRandom3 rng);
+
+        /*
+         * Determines the direction of the photomultiplier referenced by the iterator.
+         */
+        TVector3 Direction(SignalIterator current);
+
+        /*
+         * Returns an object for iterating through the pixels.
+         */
+        SignalIterator Iterator();
+
+        /*
+         * Sums all bins in the channel referenced by the iterator.
+         */
+        int SumBins(SignalIterator iter);
+
+    private:
+
+        /*
+         * Determines whether the pixel at the specified indices lies within the central circle.
+         */
+        bool ValidPixel(int x_index, int y_index);
+
+        // The underlying data structure to store photon counts
         std::vector<std::vector<std::vector<int>>> photon_counts;
+
+        // A 2D vector of bools which can be used to determine which pixels are valid.
+        std::vector<std::vector<bool>> valid_pixels;
 
         // The time at the beginning of the zeroth bin
         double start_time;
@@ -31,65 +120,8 @@ namespace cherenkov_simulator
         // The angle viewed by each photomultiplier
         double pmt_angular_size;
 
-        bool ValidPixel(int x_index, int y_index);
-        
-    public:
-
-        class SignalIterator
-        {
-
-            friend class PhotonCount;
-
-        private:
-
-            int x_current;
-
-            int y_current;
-
-            std::vector<std::vector<bool>> nonempty_pixels;
-
-            SignalIterator();
-
-            SignalIterator(std::vector<std::vector<bool>> pixel_map);
-
-        public:
-
-            int X();
-
-            int Y();
-
-            /*
-             * Moves to the next photomultiplier signal. Returns false if the iterator has reached the end of the
-             * collection (the last photomultiplier).
-             */
-            bool Next();
-
-            void Reset();
-
-        };
-
-        /*
-         * Creates a 2d indexed collection of vectors (a vector of vectors of vectors). Each vector represents the time
-         * series for a particular photomultiplier. The pixel_count parameter defines the width/height of the cluster in
-         * number of pixels.
-         */
-        PhotonCount(int n_pmt_across, double start_time, double time_bin, double pmt_angular_size);
-
-        void AddPhoton(double time, TVector3 direction);
-
-        void AddNoise(double noise_rate, SignalIterator current);
-
-        TVector3 PixelDirection(int x_index, int y_index);
-
-        /*
-         * Returns an object for iterating through the pixels.
-         */
-        SignalIterator Iterator();
-
-        /*
-         * Sums all bins in the channel specified by the iterator.
-         */
-        double SumBins(SignalIterator iter);
+        // The side length of each photomultiplier
+        double pmt_linear_size;
     };
 }
 
