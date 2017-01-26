6e1811e8f5e243668082f22dc13226f445b2ad6c
Matthew Dutson <mattdutson@icloud.com>
2017-01-26 13:13:33 -0700

    Added some tests for data containers

---
+++ cherenkov_tests/test_data_containers.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Matthew Dutson on 1/26/17.
+//
+
+#include <gtest/gtest.h>
+#include <TFile.h>
+
+#include "data_containers.h"
+#include "test_helper.h"
+#include "data_analysis.h"
+
+using namespace cherenkov_library;
+
+namespace cherenkov_tests
+{
+    /*
+     * Outputs a 2D histogram to a file. This histogram will contain a map of valid pixels.
+     */
+    TEST(valid, make_valid_map)
+    {
+        PhotonCount counter = PhotonCount(50, 0.0, 10.0e-9, 0.007, 1.0);
+        TH2C map = GetValidMap(counter);
+        TFile file("../../cherenkov_tests/pixel_map.root", "RECREATE");
+        map.Write("valid");
+    }
+}
